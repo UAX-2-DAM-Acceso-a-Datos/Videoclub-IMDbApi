74500a3d96e15f68d7947c74a646f5fe24e58a0b
sergioromerohd <sergio.romero.hombre@gmail.com>
2023-02-11 17:09:05 +0100

    Favoritos DTO

---
+++ src/main/java/com/uax/accesodatos/dto/FavoritosDto.java
@@ -0,0 +1,35 @@
+package com.uax.accesodatos.dto;
+
+public class FavoritosDto {
+	
+	private int id_usuario;
+	private String id;
+	
+	
+	
+	public FavoritosDto(int id_usuario, String id) {
+		this.id_usuario = id_usuario;
+		this.id = id;
+	}
+	
+	public int getId_usuario() {
+		return id_usuario;
+	}
+	public void setId_usuario(int id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "FavoritosDto [id_usuario=" + id_usuario + ", id=" + id + "]";
+	}
+	
+	
+
+}
