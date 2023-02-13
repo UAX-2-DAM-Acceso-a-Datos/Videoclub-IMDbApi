9675d614a53af74cdb54dbb03225f5e5e38f5d20
sergioromerohd <sergio.romero.hombre@gmail.com>
2023-02-13 11:28:16 +0100

    no message

--- src/main/java/com/uax/accesodatos/mapper/PeliculasRowMapper.java
+++ src/main/java/com/uax/accesodatos/mapper/PeliculasRowMapper.java
@@ -9,14 +9,14 @@ public class PeliculasRowMapper implements RowMapper<PeliculasDto>{
         @Override
         public PeliculasDto mapRow(java.sql.ResultSet rs, int rowNum) throws java.sql.SQLException {
                 PeliculasDto pelicula = new PeliculasDto();
-                pelicula.setId(rs.getString(1));
-                pelicula.setTitulo(rs.getString(2));
-                pelicula.setPlot(rs.getString(3));
-                pelicula.setImagen(rs.getString(4));
-                pelicula.setImDbRating(rs.getString(5));
-                pelicula.setTrailer(rs.getString(6));
-                pelicula.setRuntimeStr(rs.getString(7));
-                pelicula.setPegi(rs.getString(8));
+                pelicula.setId(rs.getString(0));
+                pelicula.setTitulo(rs.getString(1));
+                pelicula.setPlot(rs.getString(2));
+                pelicula.setImagen(rs.getString(3));
+                pelicula.setImDbRating(rs.getString(4));
+                pelicula.setTrailer(rs.getString(5));
+                pelicula.setRuntimeStr(rs.getString(6));
+                pelicula.setPegi(rs.getString(7));
                 return pelicula;
         }
     
