7c70dc5de15508eab8dd8c358b5df35761088acd
CTG099 <carlostorresgragera@gmail.com>
2023-02-13 12:43:31 +0100

    PeliculasRepo

---
+++ src/main/java/com/uax/accesodatos/repository/PeliculasRepository.java
@@ -0,0 +1,34 @@
+package com.uax.accesodatos.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.uax.accesodatos.dto.PeliculasDto;
+import com.uax.accesodatos.mapper.PeliculasRowMapper;
+
+public class PeliculasRepository {
+
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	public List<PeliculasDto> getAllPeliculas(){
+		return jdbcTemplate.query("SELECT *  FROM peliculas", new PeliculasRowMapper());
+	}
+	
+	public  boolean savePeliculas(PeliculasDto peliculas) {
+		
+		try {
+			String sql=String.format("INSERT INTO Peliculas VALUES('%d','%d','%d','%d','%d','%d','%d','%d')",
+					peliculas.getId(),peliculas.getTitulo(),peliculas.getPlot(),peliculas.getImagen(),peliculas.getImDbRating(),
+					peliculas.getTrailer(),peliculas.getRuntimeStr(),peliculas.getPegi());
+		} catch (Exception e) {
+			// TODO: handle exception
+			return false;
+		}
+		return true;
+		
+	}
+}
