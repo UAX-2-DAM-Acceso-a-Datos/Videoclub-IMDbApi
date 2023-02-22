0488ea07b9c0f04d7909be0482e487c2c573a342
HECTOR-LENOVO\Héctor Blanco <hblan@myuax.com>
2023-02-23 00:42:01 +0100

    Revert "Revert "Cambios Favoritos y Buscador definitivos""
    This reverts commit 86cb7a83fff43cdc948c662820fbaece23812315.

--- src/main/java/com/uax/accesodatos/services/Top250Service.java
+++ src/main/java/com/uax/accesodatos/services/Top250Service.java
@@ -12,7 +12,7 @@ import com.uax.accesodatos.dto.RankingResponseDto;
 @Service
 public class Top250Service {
 
-	private final String uricallTop250 = "https://imdb-api.com/en/API/MostPopularMovies/k_u5ruw4jp";
+	private final String uricallTop250 = "https://imdb-api.com/en/API/MostPopularMovies/k_4an5t72z";
 	private RestTemplate resT = new RestTemplate(); // Objeto que permite hacer llamadas de API
 	private String result;
 
